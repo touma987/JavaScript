b9aa83f5f5201cdd7295968522a4d3fdec2ed7ca
Jorgc <sheken_666@hotmail.com>
2020-05-20 03:17:46 -0300

    First commit

---
+++ msg.js
@@ -0,0 +1,10 @@
+var value=100; //Tambien puede ser declarado como window.value=100; y es global
+
+function msg(){
+    alert("Hello World");
+    document.write("GG");
+    window.value2=200; //Tambien se puede definir como global dentro de una funcion
+}
+function a(){
+    alert(value2);//Accedo variable global de la funcion msg
+}
